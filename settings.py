dab985ea311609ac8245cf0e4905c6c3160f0e54
Chaojie Yan <ushareroses@gmail.com>
2024-12-16 17:11:15 +0800

    feat: run task as cron job

--- settings.py
+++ settings.py
@@ -16,6 +16,13 @@ SGCC_ACCOUNT_USERNAME = 'admin'
 SGCC_ACCOUNT_PASSWORD = 'admin'
 
 
+POLL_INTERVAL = 5
+# the time when executing daily task
+# 24-hour system
+# 'HH:SS' format
+DAILY_CRON_TIME = '06:00'
+
+
 try:
     from settings_local import *
 except ImportError:
